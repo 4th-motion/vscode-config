baa11c7ba0dfbdd9a604670b6343842c3c945ece
Marco Koenen <mako@4thmotion.com>
2020-12-03 17:22:43 +0100

    fixed bug when no old configuration is present

--- index.js
+++ index.js
@@ -12,13 +12,15 @@ if (!fs.existsSync(VSCODE_FOLDERNAME)) {
   })
 }
 
-const getContent = (name, filePath) => {
+const getContent = (name, filePath, showError = true) => {
   try {
     return fs.readFileSync(filePath, 'utf-8')
   } catch (error) {
-    if (error.code === 'ENOENT') {
+    if (error.code === 'ENOENT' && showError) {
       console.error(chalk`{bold.red [ERROR]  } no ${name}.json was found in {underline ${filePath}}.`)
       process.exit(1)
+    } else {
+      return null
     }
   }
 }
@@ -33,12 +35,16 @@ files.forEach(async (name, index) => {
 
   if (src === dest) return
 
-  // merge both json files and overwrite old settings
-  const newContent = JSON.parse(getContent(name, src))
-  const oldContent = JSON.parse(getContent(name, dest))
-  const newJson = Object.assign(oldContent, newContent)
+  // merge both json files and overwrite old settings when possible
+  const newContent = getContent(name, src)
+  const oldContent = getContent(name, dest, false)
 
-  fs.writeFileSync(dest, JSON.stringify(newJson, null, '  '), 'utf-8')
+  const newJSON = newContent ? JSON.parse(newContent) : null
+  const oldJSON = oldContent ? JSON.parse(oldContent) : null
+
+  const mergedJSON = oldJSON ? Object.assign(oldJSON, newJSON) : newJSON
+
+  fs.writeFileSync(dest, JSON.stringify(mergedJSON, null, '  '), 'utf-8')
 
   if (index === files.length - 1) {
     console.log(chalk`{bold.green [SUCCESS]} VSCode settings imported!`)
